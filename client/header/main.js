b4ab25cd42f20b901d70af9803a3c841126c7bfe
Rowoon Lee <dev.rowoon@gmail.com>
2023-01-31 17:17:33 +0900

    [추가] 헤더 파일 붙여넣기

--- client/header/main.js
+++
@@ -1,6 +0,0 @@
-let button = document.querySelector('.AdClose');
-function handler(){
-  let AD = document.querySelector('.TopAd');
-  AD.style.display = 'none';
-}
-button.addEventListener('click',handler);
