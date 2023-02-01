3a60d882bb6840c0881e4f6247cff36b71c86555
Seung taek Oh <elqkddl@naver.com>
2023-02-01 17:23:21 +0900

    swiper-slide 수정 및 css수정#41

--- client/js/swiper.js
+++ client/js/swiper.js
@@ -2,7 +2,6 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 4, // 슬라이드 갯수 나누기
   spaceBetween: 30, // 슬라이드 사이 간격
   slidesPerGroup: 4, // 슬라이드 4개씩 들어가기
-  loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
   speed: 500,
   pagination: {
     el: '.swiper-pagination', // 버튼을 담을 태그 설정
