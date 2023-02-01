ac5ac3848ad2df19914cead9e73dbf89ae1d32ff
guahyoung <kooay93@gmail.com>
2023-02-01 13:05:22 +0900

    [추가] hover effect #44

--- client/js/nav.js
+++ client/js/nav.js
@@ -1,31 +1,42 @@
 // hover effect
+// let categoryBtn = document.querySelector('.category');
+// let categoryList = document.querySelector('.category-list');
+
+// categoryBtn.addEventListener('mouseover', function() {
+//   categoryList.style.display = 'block';
+// })
+
+// categoryBtn.addEventListener('mouseout', function() {
+//   categoryList.style.display = 'none';
+// })
+
+// categoryList.addEventListener('mouseover', function() {
+//   categoryList.style.display = 'block';
+// })
+
+// categoryList.addEventListener('mouseout', function() {
+//   categoryList.style.display = 'none';
+// })
+
+
 let categoryBtn = document.querySelector('.category');
 let categoryList = document.querySelector('.category-list');
 
-categoryBtn.addEventListener('mouseover', function() {
-  categoryList.style.display = 'block';
-})
+function hoverCategoryList() {
+  categoryList.style.display = (categoryList.style.display === 'block') ? 'none' : 'block';
+}
 
-categoryBtn.addEventListener('mouseout', function() {
-  categoryList.style.display = 'none';
-})
-
-categoryList.addEventListener('mouseover', function() {
-  categoryList.style.display = 'block';
-})
-
-categoryList.addEventListener('mouseout', function() {
-  categoryList.style.display = 'none';
-})
-
+categoryBtn.addEventListener('mouseover', hoverCategoryList);
+categoryBtn.addEventListener('mouseout', hoverCategoryList);
+categoryList.addEventListener('mouseover', hoverCategoryList);
+categoryList.addEventListener('mouseout', hoverCategoryList);
 
 
 // let mainNav = document.querySelectorAll('.nav-center');
 // let smallNav = document.querySelectorAll('.nav-small');
 
-// document.body.scrollTop >= 0 ? smallNav.style.display = "flex" : mainNav.style.display = "flex";
 
-
+// document.scrollingElement.scrollTop; >= 0 ? smallNav.style.display = "flex" : mainNav.style.display = "flex";
 
 
 // 스크롤
