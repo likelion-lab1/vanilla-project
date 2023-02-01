9355bb5d7acd98839a39596692dbc5efc2d0e4c1
guahyoung <kooay93@gmail.com>
2023-02-01 17:31:58 +0900

    [수정] hover effect #44

--- client/js/nav.js
+++ client/js/nav.js
@@ -1,28 +1,7 @@
-// hover effect
-// let categoryBtn = document.querySelector('.category');
-// let categoryList = document.querySelector('.category-list');
-
-// categoryBtn.addEventListener('mouseover', function() {
-//   categoryList.style.display = 'block';
-// })
-
-// categoryBtn.addEventListener('mouseout', function() {
-//   categoryList.style.display = 'none';
-// })
-
-// categoryList.addEventListener('mouseover', function() {
-//   categoryList.style.display = 'block';
-// })
-
-// categoryList.addEventListener('mouseout', function() {
-//   categoryList.style.display = 'none';
-// })
-
-
 let categoryBtn = document.querySelector('.category');
 let categoryList = document.querySelector('.category-list');
 
-function hoverCategoryList() {
+function hoverCategoryList(e) {
   categoryList.style.display = (categoryList.style.display === 'block') ? 'none' : 'block';
 }
 
@@ -32,11 +11,17 @@ categoryList.addEventListener('mouseover', hoverCategoryList);
 categoryList.addEventListener('mouseout', hoverCategoryList);
 
 
-// let mainNav = document.querySelectorAll('.nav-center');
-// let smallNav = document.querySelectorAll('.nav-small');
+let smallCategoryBtn = document.querySelector('.small-category');
+let smallCategoryList = document.querySelector('.small-category-list');
 
+function smallHoverCategoryList(e) {
+  smallCategoryList.style.display = (smallCategoryList.style.display === 'block') ? 'none' : 'block';
+}
 
-// document.scrollingElement.scrollTop; >= 0 ? smallNav.style.display = "flex" : mainNav.style.display = "flex";
+smallCategoryBtn.addEventListener('mouseover', smallHoverCategoryList);
+smallCategoryBtn.addEventListener('mouseout', smallHoverCategoryList);
+smallCategoryList.addEventListener('mouseover', smallHoverCategoryList);
+smallCategoryList.addEventListener('mouseout', smallHoverCategoryList);
 
 
 // 스크롤
@@ -57,3 +42,33 @@ function scrollHandeler(e){
 }
 
 window.addEventListener('scroll',scrollHandeler)
+
+
+
+// hover effect
+// let categoryBtn = document.querySelector('.category');
+// let categoryList = document.querySelector('.category-list');
+
+
+// categoryBtn.addEventListener('mouseover', function() {
+  //   categoryList.style.display = 'block';
+  // })
+  
+  // categoryBtn.addEventListener('mouseout', function() {
+    //   categoryList.style.display = 'none';
+    // })
+    
+    // categoryList.addEventListener('mouseover', function() {
+      //   categoryList.style.display = 'block';
+      // })
+      
+      // categoryList.addEventListener('mouseout', function() {
+        //   categoryList.style.display = 'none';
+        // })
+
+
+        // let mainNav = document.querySelectorAll('.nav-center');
+        // let smallNav = document.querySelectorAll('.nav-small');
+        
+        
+        // document.scrollingElement.scrollTop; >= 0 ? smallNav.style.display = "flex" : mainNav.style.display = "flex";
