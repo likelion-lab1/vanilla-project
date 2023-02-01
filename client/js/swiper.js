e5ddb33be1522e1e3bccb5e9998c134d03fde0c9
Seung taek Oh <elqkddl@naver.com>
2023-02-01 23:10:34 +0900

    swiper button class 추가#41

--- client/js/swiper.js
+++ client/js/swiper.js
@@ -1,7 +1,9 @@
 const swiper = new Swiper('.swiper', {
   slidesPerView: 4, // 슬라이드 갯수 나누기
   spaceBetween: 30, // 슬라이드 사이 간격
-  slidesPerGroup: 4, // 슬라이드 4개씩 들어가기
+  slidesPerGroup: 4,// 슬라이드 4개씩 들어가기
+  watchOverflow : true ,
+  observer: true, 
   speed: 500,
   pagination: {
     el: '.swiper-pagination', // 버튼을 담을 태그 설정
