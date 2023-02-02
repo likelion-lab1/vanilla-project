2a0839d252b4801915e4a13e3691c9638378f867
Rowoon Lee <dev.rowoon@gmail.com>
2023-02-02 23:07:18 +0900

    [수정] 주석 제거

--- client/js/cookie.js
+++ client/js/cookie.js
@@ -1,47 +1,9 @@
-// 쿠키 생성 유틸함수 만들기
-// function setCookie(name,value,day){
-//   // 지금으로부터 하루 후
-//   let date = new Date(Date.now() + day);
-//   date = date.toUTCString();
-//   document.cookie = name + '=' + value; 'expires=' + date;
-// }
-// setCookie('user','mike',86400e3)
-
-// 우리 사이트에 들어왔었는지 확인을 해야하기 때문에 만드는 유틸함수
-// 원하는 쿠키가 있는지 확인(내가 원하는 name과 value가 있는지 확인)
-// 특정문자를 기준으로 배열생성(.split)
-// function getCookie(name) {
-//   let cookies = document.cookie.split(';');
-
-//   for(let i in cookies){
-
-//     if(cookies[i].indexOf(name) > -1){
-//       console.log(cookies[i]);
-//     }
-//   }
-
-// }
-
-// getCookie('mike');
-
-
-// 쿠키 삭제
-
-
-
 // 작업시작
 let popupButtonLeft = document.querySelector('.popupButtonLeft');
-// let popupImg = document.querySelector('.popupImg');
-// let popupLink = document.querySelector('.popupLink');
-// let popupButton = document.querySelector('.popupButton');
 let popup = document.querySelector('.popup');
 
 
 function setCookie(name,value,day){
-  // 지금으로부터 하루 후
-  // let date = new Date(Date.now() + day);
-  // date = date.toUTCString();
-  // document.cookie = name + '=' + value; 'expires=' + date;
 
   let date = new Date();
   date.setDate(date.getDate() + day);
@@ -53,7 +15,6 @@ function setCookie(name,value,day){
   document.cookie = mycookie
   
 }
-// setCookie('user','tom',1)
 
 //쿠키 삭제
 function delCookie(name){
