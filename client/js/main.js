4acd6959e64fe9932838bf07824435e8bb1db97e
Seung taek Oh <elqkddl@naver.com>
2023-02-02 21:45:45 +0900

    ㅁ

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-
-=======
 let button = document.querySelector('.AdClose');
 function handler(){
   let AD = document.querySelector('.TopAd');
   AD.style.display = 'none';
 }
 button.addEventListener('click',handler);
->>>>>>> f1ba02e52f0c481180f6ef4d22aa958f2f6dd775
--- client/login,reg/main.js
+++ client/login,reg/main.js
@@ -1,7 +1,14 @@
 // 전체 체크 전체 체크 해제
 
+import { getNode } from "../lib/index.js";
+
 let allCheck = document.querySelector('.all-check');
 let subCheck = document.querySelectorAll('.sub-check');
+let idCheck = document.querySelectorAll('.button-size')
+
+
+
+console.log()
 
 function checkhandler(){
   
@@ -16,3 +23,4 @@ function checkhandler(){
 }
 
 allCheck.addEventListener('click',checkhandler)
+
