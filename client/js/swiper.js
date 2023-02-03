967fbf86cfc88bcc22aa78a728ac21e9db5aee01
Eonii2 <eoniioff@gmailcom>
2023-02-03 15:12:40 +0900

    [추가] 최근 본 상품 영역 스와이퍼 기능 추가 #54

--- client/js/swiper.js
+++ client/js/swiper.js
@@ -1,6 +1,6 @@
 const swiper1 = new Swiper(".recommend-list-swiper", {
   slidesPerView: 4, // 슬라이드 갯수 나누기
-  spaceBetween: 30, // 슬라이드 사이 간격
+  spaceBetween: 18, // 슬라이드 사이 간격
   slidesPerGroup: 4, // 슬라이드 4개씩 들어가기
   loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
   speed: 500,
@@ -10,8 +10,9 @@ const swiper1 = new Swiper(".recommend-list-swiper", {
   },
   navigation: {
     // 버튼
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+
+    nextEl: ".recommend-list-next",
+    prevEl: ".recommend-list-prev",
   },
   keyboard: {
     enabled: true,
@@ -21,7 +22,7 @@ const swiper1 = new Swiper(".recommend-list-swiper", {
 
 const swiper2 = new Swiper(".special-deal-list-siwper", {
   slidesPerView: 4, // 슬라이드 갯수 나누기
-  spaceBetween: 30, // 슬라이드 사이 간격
+  spaceBetween: 18, // 슬라이드 사이 간격
   slidesPerGroup: 4, // 슬라이드 4개씩 들어가기
   loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
   speed: 500,
@@ -39,10 +40,11 @@ const swiper2 = new Swiper(".special-deal-list-siwper", {
     onlyInViewport: false,
   },
 });
-const swiper3 = new Swiper(".special-deal-list-siwper", {
-  slidesPerView: 4, // 슬라이드 갯수 나누기
-  spaceBetween: 30, // 슬라이드 사이 간격
-  slidesPerGroup: 4, // 슬라이드 4개씩 들어가기
+const swiper3 = new Swiper(".recent-product-swiper", {
+  direction: "vertical",
+  slidesPerView: 1, // 슬라이드 갯수 나누기
+  spaceBetween: 8, // 슬라이드 사이 간격
+  slidesPerGroup: 1, // 슬라이드 4개씩 들어가기
   loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
   speed: 500,
   pagination: {
@@ -51,8 +53,8 @@ const swiper3 = new Swiper(".special-deal-list-siwper", {
   },
   navigation: {
     // 버튼
-    nextEl: ".special-deal-list-next",
-    prevEl: ".special-deal-list-prev",
+    nextEl: ".recent-list-button-next",
+    prevEl: ".recent-list-button-prev",
   },
   keyboard: {
     enabled: true,
