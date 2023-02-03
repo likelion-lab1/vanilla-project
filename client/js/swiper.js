8bc4cd01576374f41023dd41b3f1c685db91786e
Eonii2 <eoniioff@gmailcom>
2023-02-03 11:03:37 +0900

    [추가] 스와이퍼 버튼 키보드 탭기능 추가 #67

--- client/js/swiper.js
+++ client/js/swiper.js
@@ -13,6 +13,10 @@ const swiper1 = new Swiper(".recommend-list-swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
 
 const swiper2 = new Swiper(".special-deal-list-siwper", {
@@ -29,5 +33,29 @@ const swiper2 = new Swiper(".special-deal-list-siwper", {
     // 버튼
     nextEl: ".special-deal-list-next",
     prevEl: ".special-deal-list-prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+const swiper3 = new Swiper(".special-deal-list-siwper", {
+  slidesPerView: 4, // 슬라이드 갯수 나누기
+  spaceBetween: 30, // 슬라이드 사이 간격
+  slidesPerGroup: 4, // 슬라이드 4개씩 들어가기
+  loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
+  speed: 500,
+  pagination: {
+    el: ".swiper-pagination", // 버튼을 담을 태그 설정
+    type: "fraction", // 기본 슬라이더를 fraction으로 변경
+  },
+  navigation: {
+    // 버튼
+    nextEl: ".special-deal-list-next",
+    prevEl: ".special-deal-list-prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
   },
 });
