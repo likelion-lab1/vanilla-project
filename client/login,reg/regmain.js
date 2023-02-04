81138dd372e34e74d26eddcef379ea59b13c4a79
Seung taek Oh <elqkddl@naver.com>
2023-02-05 01:55:31 +0900

    비밀번호 유효성 검사 실패#51

--- client/login,reg/regmain.js
+++ client/login,reg/regmain.js
@@ -1,15 +1,14 @@
 // 전체 체크 전체 체크 해제
 
-import { getInputValue, getNode, getNodes, } from "../lib/index.js"
+import { getInputValue, getNode, getNodes,showAlert,typeError } from "../lib/index.js"
 
+
+// 전체항목 선태 
 let allCheck = getNode('.all-check');
 let subCheck = getNodes('.sub-check');
 let submitCheck = getNode('#submit1')
 
 // let passwordCheck = getNode('#submit-password')
-
-const REGPASSSWORD = /(?=.*\d{1,50})(?=.*[~`!@#$%&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,20}$/;
-
 function checkhandler(){
   
   if(allCheck.checked){
@@ -22,13 +21,15 @@ function checkhandler(){
   }
 }
 
+
+// 아이디 인증버튼
+const REGISTERID = /^[a-z]+[a-z0-9]{5,19}$/g
 function clickIdHandler(e) {
   e.preventDefault();
   let name = getInputValue('#nameField')
   if(!name){
     alert('아이디를 입력해주세요')
     return
-
   }
   if(name.length<6){
     alert('6글자 이상 입력해주세요')
@@ -37,37 +38,41 @@ function clickIdHandler(e) {
     return
   }
 }
-allCheck.addEventListener('click',checkhandler)
 
 
-submitCheck.addEventListener('click',clickIdHandler)
+// 이메일 검사
+
+
+
+
 // 활성화버튼
 const register = document.querySelector('.register');
-
-let pw = document.querySelector('.pw-input');
+let password = document.querySelector('.pw-input');
 let email = document.querySelector('.email-input');
 let resgisterButton =document.querySelector('.register-button');
 
-
-function keycheck(e){
-  e = e.target
-  // if(e ==pw){
-  //   console.log('pw');
-  // }
-  // if(e == email){
-  //   console.log('eamil')
-  // }
- 
-  if (pw.value.length >= 8&&email.value.includes('@') && email.value.includes('.') )
-  {
-    // disable 쓰지마세요
-    resgisterButton.disabled = false
-  } else{
+function registerCheck(e){
+  e.preventDefault();
+  if (!password){
+    alert('password를 입력해주세요')
+  }
+  if(password.value.length > 8 ){
+    alert('비밀번호를 8자리 이상 입력해주세요')
+    return;
+  }
+  if (password.value.length< 8 || password.value.length>20){
     resgisterButton.disabled = true
   }
 }
 
-register.addEventListener('keyup',keycheck)
+
+
+
+
+// addEventListener('keyup',passwordCheck)
+register.addEventListener('keyup',registerCheck)
+allCheck.addEventListener('click',checkhandler)
+submitCheck.addEventListener('click',clickIdHandler)
 // document.querySelector('.pw-input').value.length>=9
 // document.querySelector('.email-input').value.includes('@')
 // document.querySelector('.email-input').value.includes(',')
