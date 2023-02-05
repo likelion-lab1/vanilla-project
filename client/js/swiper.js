3abb578afffff6d4464e045ed3654722d6ea414e
Eonii2 <eoniioff@gmailcom>
2023-02-05 21:23:43 +0900

    [수정] 최근 본 상품 스와이퍼 overflow 문제  해결 #54

--- client/js/swiper.js
+++ client/js/swiper.js
@@ -42,17 +42,10 @@ const swiper2 = new Swiper(".special-deal-list-siwper", {
 });
 const swiper3 = new Swiper(".recent-product-swiper", {
   direction: "vertical",
-  slidesPerView: 1, // 슬라이드 갯수 나누기
-  spaceBetween: 8, // 슬라이드 사이 간격
-  slidesPerGroup: 1, // 슬라이드 4개씩 들어가기
-  loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
-  speed: 500,
-  pagination: {
-    el: ".swiper-pagination", // 버튼을 담을 태그 설정
-    type: "fraction", // 기본 슬라이더를 fraction으로 변경
-  },
+  slidesPerView: 2.5,
+  spaceBetween: 4,
+  speed: 450,
   navigation: {
-    // 버튼
     nextEl: ".recent-list-button-next",
     prevEl: ".recent-list-button-prev",
   },
