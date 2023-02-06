d720c253c6c399902cf93e50f99cb4ddedda5ce3
guahyoung <kooay93@gmail.com>
2023-02-06 17:13:15 +0900

    [수정] heart #72

--- client/js/productdetail.js
+++ client/js/productdetail.js
@@ -38,62 +38,13 @@ bubble.style.display = 'none';
 cartBtn.addEventListener('click',handler);
 
 
-//하트 색상 변경 
-// let heart = document.querySelector(".product-detail-heart-icon");
-// const CLICKED_CLASS = "clicked";
 
-// function handleClick() {
-//     heart.classList.toggle(CLICKED_CLASS);
-// }
+//heart color 
+let heartButton = document.querySelector('.cart-button');
+let heart = document.querySelector('.heart-path');
 
-// heart.addEventListener("click", handleClick);
+heartButton.addEventListener('click',()=>{
 
-// function init() {
-//     handleClick();
-// }
-
-// init();
-
-
-let button = document.querySelector(".product-detail-heart-icon");
-
-button.addEventListener("click", function() {
-    if (button.style.backgroundColor === "red") {
-   button.style.backgroundColor = "";
-  } else {
-   button.style.backgroundColor = "red";
-  }
-});
-
-
-
-//item count
-// const number = document.querySelector(".count-number");
-// const increase = document.querySelector(".count-button-plus");
-// const decrease = document.querySelector(".count-button-minus");
-
-// increase.onclick = () => {
-//   const current = parseInt(number.innerText, 10);
-//   number.innerText = current + 1;
-// };
-
-// decrease.onclick = () => {
-//   const current = parseInt(number.innerText, 10);
-//   number.innerText = current - 1;
-// };
-
-
-
-
-
-//총 금액 
-// let price = document.querySelector(".product-detail-itmem-price")
-
-// function sum(number, price) {
-//   let total = number * price;
-//   return total = document.querySelector(".total-price");
-//   }
   
-//   let total = sum(number, price);
-
-
+  heart.classList.toggle('fill-red')
+})
