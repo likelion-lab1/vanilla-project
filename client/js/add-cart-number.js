34404285b495e13bd5a1d0acecc265fbcfb58304
Eonii2 <eoniioff@gmailcom>
2023-02-06 10:13:24 +0900

    [추가] 숫자 증감 플러스 완료 #76

---
+++ client/js/add-cart-number.js
@@ -0,0 +1,23 @@
+let plus = document.querySelector(".add-quantiy");
+let minus = document.querySelector(".minus-quantity");
+let result = document.querySelector("#result");
+let totalCost = document.querySelector(".total-cost");
+let i = 1;
+
+plus.addEventListener("click", () => {
+  i++;
+  result.textContent = i;
+  let totalCostNum = i * 4980;
+  totalCost.textContent = totalCostNum.toLocaleString();
+});
+
+minus.addEventListener("click", () => {
+  if (i > 0) {
+    i--;
+    result.textContent = i;
+    let totalCostNum = i * 4980;
+    totalCost.textContent = totalCostNum.toLocalString();
+  } else {
+    totalCost.textContent = 0 + 1;
+  }
+});
