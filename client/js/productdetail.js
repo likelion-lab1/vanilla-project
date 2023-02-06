cd7210b43cf36d46f64e7691a7cb02c89f474a68
guahyoung <kooay93@gmail.com>
2023-02-06 15:04:59 +0900

    [수정] heart #72

--- client/js/productdetail.js
+++ client/js/productdetail.js
@@ -1,19 +1,27 @@
+// 수량 증감,총 금액 변경
+let plus = document.querySelector(".count-button-plus");
+let minus = document.querySelector(".count-button-minus");
+let result = document.querySelector(".count-number");
+let totalCost = document.querySelector(".total-price");
+let i = 1;
 
+plus.addEventListener("click", () => {
+  i++;
+  result.textContent = i;
+  let totalCostNum = i * 4980;
+  totalCost.textContent = totalCostNum.toLocaleString();
+});
 
-//item count
-const number = document.querySelector(".count-number");
-const increase = document.querySelector(".count-button-plus");
-const decrease = document.querySelector(".count-button-minus");
-
-increase.onclick = () => {
-  const current = parseInt(number.innerText, 10);
-  number.innerText = current + 1;
-};
-
-decrease.onclick = () => {
-  const current = parseInt(number.innerText, 10);
-  number.innerText = current - 1;
-};
+minus.addEventListener("click", () => {
+  if (i > 0) {
+    i--;
+    result.textContent = i;
+    let totalCostNum = i * 4980;
+    totalCost.textContent = totalCostNum.toLocaleString();
+  } else {
+    totalCost.textContent = 0 + 1;
+  }
+});
 
 
 //cart bubble
@@ -30,6 +38,53 @@ bubble.style.display = 'none';
 cartBtn.addEventListener('click',handler);
 
 
+//하트 색상 변경 
+// let heart = document.querySelector(".product-detail-heart-icon");
+// const CLICKED_CLASS = "clicked";
+
+// function handleClick() {
+//     heart.classList.toggle(CLICKED_CLASS);
+// }
+
+// heart.addEventListener("click", handleClick);
+
+// function init() {
+//     handleClick();
+// }
+
+// init();
+
+
+let button = document.querySelector(".product-detail-heart-icon");
+
+button.addEventListener("click", function() {
+    if (button.style.backgroundColor === "red") {
+   button.style.backgroundColor = "";
+  } else {
+   button.style.backgroundColor = "red";
+  }
+});
+
+
+
+//item count
+// const number = document.querySelector(".count-number");
+// const increase = document.querySelector(".count-button-plus");
+// const decrease = document.querySelector(".count-button-minus");
+
+// increase.onclick = () => {
+//   const current = parseInt(number.innerText, 10);
+//   number.innerText = current + 1;
+// };
+
+// decrease.onclick = () => {
+//   const current = parseInt(number.innerText, 10);
+//   number.innerText = current - 1;
+// };
+
+
+
+
 
 //총 금액 
 // let price = document.querySelector(".product-detail-itmem-price")
@@ -40,3 +95,5 @@ cartBtn.addEventListener('click',handler);
 //   }
   
 //   let total = sum(number, price);
+
+
