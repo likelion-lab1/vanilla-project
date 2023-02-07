6e2a062b08ed692d05e45db74bb214605785d99d
guahyoung <kooay93@gmail.com>
2023-02-07 14:10:45 +0900

    [추가] add-cart

--- client/js/productlist.js
+++ client/js/productlist.js
@@ -46,3 +46,19 @@ let han = ()=>{
 }
 
 initializationButton.addEventListener('click',han)
+
+
+
+const openButton = document.querySelector(".best-page-cartButton");
+const container = document.querySelector(".add-cart-hidden");
+const closeButton = document.querySelector("#close");
+
+openButton.addEventListener("click", () => {
+  container.style.display = "flex";
+  // openButton.style.display = "none";
+});
+
+closeButton.addEventListener("click", () => {
+  container.style.display = "none";
+  openButton.style.display = "block";
+});
