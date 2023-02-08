e9341d5de41fd5f977ef82bcacc3bf06c43d8efa
Eonii2 <eoniioff@gmailcom>
2023-02-08 11:56:36 +0900

    [추가] 오버레이 클릭 시 close 되는 코드 추가 #75

--- client/js/modal-add-cart.js
+++ client/js/modal-add-cart.js
@@ -1,13 +1,19 @@
 const openButton = document.querySelector("#cart1");
 const container = document.querySelector(".add-cart-hidden");
+const addCartOverlay = document.querySelector(".add-cart-overlay");
 const closeButton = document.querySelector("#close");
 
 openButton.addEventListener("click", () => {
   container.style.display = "flex";
+  container.focus();
   // openButton.style.display = "none";
 });
 
 closeButton.addEventListener("click", () => {
+  container.style.display = "none";
+  openButton.style.display = "block";
+});
+addCartOverlay.addEventListener("click", () => {
   container.style.display = "none";
   openButton.style.display = "block";
 });
